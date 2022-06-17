b8466bbd645c16f07cdf58cdcbe5b72120cdfb75
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-06-17 16:00:09 +0900

    3.3 Apollo server

---
+++ server.js
@@ -0,0 +1,42 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));

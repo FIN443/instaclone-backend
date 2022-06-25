8e500141f150b5db4a04314f2de6b78b1432981e
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-06-25 14:36:40 +0900

    updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,40 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let hashPassword = null;
+      if (newPassword) {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        hashPassword = await bcrypt.hash(newPassword, salt);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(hashPassword && { password: hashPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile.",
+        };
+      }
+    },
   },
 };
